4c02c60871591802682e3bd1f07e9e004e016a63
justus.westerhoff <justus.westerhoff@gmail.com>
2025-04-09 19:50:13 +0200

    change to 7b and add vision tower variibility

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+# VISION_TOWER_ORIGIN=openai
+# VISION_TOWER=clip-vit-large-patch14-336
+VISION_TOWER_ORIGIN=UCSC-VLAA
+VISION_TOWER=ViT-L-14-CLIPA-336-datacomp1B
+
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version plain \
-    --data_path ./playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
-    --image_folder ./playground/data/LLaVA-Pretrain/images \
-    --vision_tower openai/clip-vit-large-patch14-336 \
+    --data_path /llava-datasets/pretrain_feature_alignment/blip_laion_cc_sbu_558k.json \
+    --image_folder /llava-datasets/pretrain_feature_alignment \
+    --vision_tower $VISION_TOWER_ORIGIN/$VISION_TOWER \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-pretrain \
+    --output_dir /llava-checkpoints/llava-v1.5-7b-$VISION_TOWER_ORIGIN-$VISION_TOWER-bs16-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
